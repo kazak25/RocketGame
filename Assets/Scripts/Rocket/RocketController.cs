41d2db3abd341ac14544454389deb880b980c464
kazak25 <kazak77709@gmail.com>
2023-07-17 21:08:39 +0300

    Some Refactoring

--- Assets/Scripts/Rocket/RocketController.cs
+++ Assets/Scripts/Rocket/RocketController.cs
@@ -7,14 +7,14 @@ public class RocketController : MonoBehaviour
 
     [SerializeField] private float _jumpForce = 5f;
     [SerializeField] private float _horizontalMoveSpeed = 5f;
-
-    public Rigidbody2D rb;
+    [SerializeField ]private Rigidbody2D _rb;
+    
     private bool isJumping = false;
-    private float horizontalMovement = 0f;
+    private float _horizontalMovement = 0f;
 
     void Update()
     {
-        horizontalMovement = 0f;
+        _horizontalMovement = 0f;
         if (Input.touchSupported)
         {
             if (Input.touchCount > 0)
@@ -26,17 +26,15 @@ public class RocketController : MonoBehaviour
                     if (touch.phase == TouchPhase.Began || touch.phase == TouchPhase.Stationary)
                     {
                         isJumping = true;
-                        rb.velocity = Vector2.up * _jumpForce;
+                        _rb.velocity = Vector2.up * _jumpForce;
                     }
                     else if (touch.phase == TouchPhase.Ended)
                     {
                         isJumping = false;
-                        
-                        //horizontalMovement = 0f;
                     }
                     else if (touch.phase == TouchPhase.Moved)
                     {
-                        horizontalMovement = touch.deltaPosition.normalized.x * _horizontalMoveSpeed;
+                        _horizontalMovement = touch.deltaPosition.normalized.x * _horizontalMoveSpeed;
                     }
                 }
             }
@@ -46,8 +44,8 @@ public class RocketController : MonoBehaviour
             if (Input.GetKey(KeyCode.Space))
             {
                 isJumping = true;
-                rb.velocity = Vector2.up * _jumpForce;
-                horizontalMovement = Input.GetAxis("Horizontal") * _horizontalMoveSpeed;
+                _rb.velocity = Vector2.up * _jumpForce;
+                _horizontalMovement = Input.GetAxis("Horizontal") * _horizontalMoveSpeed;
             }
             else
             {
@@ -60,8 +58,8 @@ public class RocketController : MonoBehaviour
     {
         if (isJumping)
         {
-            Vector2 movement = new Vector2(horizontalMovement, rb.velocity.y);
-            rb.velocity = movement;
+            Vector2 movement = new Vector2(_horizontalMovement, _rb.velocity.y);
+            _rb.velocity = movement;
         }
     }
 
